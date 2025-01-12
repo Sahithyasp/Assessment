c6bf8bb0bffc2fd985b58b01c51278c077e51e6c
sahithya <69206146+Sahithyasp@users.noreply.github.com>
2025-01-12 19:18:28 +0530

    Create validation.sql

---
+++ validation.sql
@@ -0,0 +1,33 @@
+// validation to check all the mandatory fields are not blank
+SELECT *
+FROM Staging_Table
+WHERE Customer_Name IS NULL 
+OR Customer_ID IS NULL 
+OR Open_Date IS NULL;
+
+// validation to check duplicate customerids from the staging table
+
+SELECT Customer_ID, COUNT(*)
+FROM Stage_Table
+WHERE Record_Type = 'D'
+GROUP BY Customer_ID
+HAVING COUNT(*) > 1;
+
+// validation to check only the active customers are loaded in the table
+
+SELECT * 
+FROM Stage_Table
+WHERE Act_Cust = 'A';
+
+//validation to check the date formats are same in the staging table while loading the data
+
+SELECT * 
+FROM Stage_Table
+WHERE TRY_TO_DATE(Open_Date, 'YYYYMMDD') IS NULL;
+
+
+
+
+
+
+
